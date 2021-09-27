78e5b971052276947c58d62ae86dfb870e4b5ddb
Ethan Jolles <85588248+EthanJolles@users.noreply.github.com>
2021-09-26 23:32:59 -0400

    Update div7mul5.py

--- div7mul5.py
+++ div7mul5.py
@@ -1,7 +1,7 @@
-#
-Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
-between 2000 and 3200 (both included).
-The numbers obtained should be printed in a comma-separated sequence on a single line.
+# https://github.com/zhiwehu/Python-programming-exercises/blob/master/100%2B%20Python%20challenging%20programming%20exercises.txt
+# Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+# between 2000 and 3200 (both included).
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
 
 
 # zhiwehu's solution
